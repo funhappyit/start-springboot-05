db26d800fc6d522a368eded6f9c464af2aeb63cc
funhappyit <fun.happyit@gmail.com>
2021-09-22 17:52:26 +0900

    2021-09-22 1) 검색 test

---
+++ src/main/java/org/zerock/persistence/WebBoardRepository.java
@@ -0,0 +1,28 @@
+package org.zerock.persistence;
+
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.QWebBoard;
+import org.zerock.domain.WebBoard;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+
+public interface WebBoardRepository extends CrudRepository<WebBoard, Long>,
+											QuerydslPredicateExecutor<WebBoard>{
+	
+	public default Predicate makePredicate(String type, String keyword) {
+		BooleanBuilder builder = new BooleanBuilder();
+		
+		QWebBoard board = QWebBoard.webBoard;
+		
+		//type if ~ else
+		
+		// bno>0
+		builder.and(board.bno.gt(0));
+		
+		return builder;
+		
+	}
+
+}
